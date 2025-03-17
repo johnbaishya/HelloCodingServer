24184dc47f6df395d9d221133ee016934257fdf7
John Baishya <baishyajohn@gmail.com>
2025-03-17 15:44:16 +1100

    allow cors for all

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,8 @@ const app: Application = express();
 
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4001');
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:4001');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept'
